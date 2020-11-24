ea12441535a522cead7e27e5a6d23e8642bcf392
Truman <56100185+trumanbe01@users.noreply.github.com>
2020-11-24 16:29:39 -0500

    Added components for profile page

--- QTMAProject/src/screens/Drawer/settings.js
+++ QTMAProject/src/screens/Drawer/settings.js
@@ -11,17 +11,60 @@ class SettingsScreen extends Component {
     return (
       <View style={styles.form}>
         <View style={styles.row}>
-          <Text style={styles.pageTitle}>{'Profile'}</Text>
+          <Text style={styles.greeting}>Hi {'Max'}.</Text>
+          <IconButton
+            icon="account-circle"
+            color="#1e90ff"
+            size={40}
+            style={{alignSelf:'center'}}
+            onPress={() => console.log('Account')}
+          />
+        </View>
+        <IconButton
+          icon="qrcode-scan"
+          color="#1e90ff"
+          size={150}
+          style={{alignSelf:'center'}}
+          onPress={() => console.log('QR Code')}
+        />
 
-          {/* <Avatar.Text size={50} label="QRF" style={styles.avatar} /> */}
-
-          {/* <IconButton
-            icon="camera"
+      <View style={{}}>
+        <View style={styles.row}>
+          <Text style={{fontWeight: 'bold',}}>Contact Information:</Text>
+          <IconButton
+            icon="pencil"
             color="#1e90ff"
             size={20}
-            onPress={() => console.log('Pressed')}
-          /> */}
+            style={{}}
+            onPress={() => console.log('Contact information edit')}
+          />
+          <IconButton
+            icon="help-circle-outline"
+            color="#1e90ff"
+            size={20}
+            style={{}}
+            onPress={() => console.log('Contact information help')}
+          />
         </View>
+        <Text style={{}}>Max Eisen</Text>
+        <Text style={{}}>613-737-1111</Text>
+        <Text style={{}}>K7L 2K5</Text>
+      </View>
+
+      <View style={{}}>
+        <View style={styles.row}>
+          <Text style={{fontWeight: 'bold',}}>Recent Locations:</Text>
+          <IconButton
+            icon="help-circle-outline"
+            color="#1e90ff"
+            size={20}
+            style={{}}
+            onPress={() => console.log('Recent locations help')}
+          />
+        </View>
+        <Text style={{}}>Lone Star Texas Grill</Text>
+        <Text style={{}}>Burger King</Text>
+      </View>
 
         <Button
           style={styles.authButton}
