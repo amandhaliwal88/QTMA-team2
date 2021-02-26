8f09581f763563797ee9a2d51035f5c59db712b0
Truman Be <trumanbe01@gmail.com>
2021-02-25 20:12:42 -0500

    Tweaked styling

--- QTMAProject/src/components/rating.js
+++ QTMAProject/src/components/rating.js
@@ -5,9 +5,19 @@ import Star from 'react-native-vector-icons/Fontisto';
 class RatingCompact extends Component {
     render() {
         return(
-            <View style={{display: 'flex', flexDirection: 'row', width: 120, height: 55, justifyContent: 'space-evenly', alignItems: 'center', backgroundColor: '#d4a265', borderRadius: 15}}>
-                <Star name='star' size={35}/>
-                <Text style={{fontSize: 30}}>{this.props.rating}/5</Text>
+            <View style={{display: 'flex', flexDirection: 'row', paddingHorizontal: 15, paddingVertical: 5, justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#d4a265', borderRadius: 15}}>
+                <Star name='star' size={30}/>
+                <Text style={{fontSize: 30}}> {this.props.rating}/5</Text>
+            </View>
+        )
+    }
+}
+
+class RatingDetailed extends Component {
+    render() {
+        return(
+            <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', padding: 10}}>
+
             </View>
         )
     }
